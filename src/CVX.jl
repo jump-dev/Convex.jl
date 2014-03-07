6f9d0d87476e90736399f89a6d3325c4d6004070
Madeleine Udell <madeleine.udell@gmail.com>
2014-03-07 14:29:13 -0800

    added code from dropbox

--- src/CVX.jl
+++ src/CVX.jl
@@ -1,5 +1,11 @@
 module CVX
 
 # package code goes here
+require("expressions.jl")
+require("arithmetic.jl")
+require("constraints.jl")
+require("problems.jl")
+require("cvxpy.jl")
+require("atoms.jl")
 
 end # module
