58453619a2cbdbce6b30cab5610b8cd7c8aa0630
Karanveer Mohan <karanveer.1992@gmail.com>
2014-04-25 23:03:56 -0700

    add asserts to test

--- test/test.jl
+++ test/test.jl
@@ -1,80 +1,77 @@
 @everywhere require("src/CVX.jl")
 @everywhere using CVX
 
-# # Test 1
-# x = Variable(1)
-# p = Problem(:minimize, -x, [x <= 0])
-# solve!(p)
+TOLERANCE = .0001
+
+# Test 1
+x = Variable(1)
+p = Problem(:minimize, -x, [x <= 0])
+solve!(p)
+@assert abs(p.optval - 0) <= TOLERANCE
 
 # # Test 2
-# x = Variable(1)
-# p = Problem(:minimize, x, [x >= 2, x <= 4])
-# solve!(p)
+x = Variable(1)
+p = Problem(:minimize, x, [x >= 2, x <= 4])
+solve!(p)
+@assert abs(p.optval - 2) <= TOLERANCE
 
-# # Test 3
-# x = Variable(1)
-# p = Problem(:minimize, 2.0 * x, [x >= 2, x <= 4])
-# solve!(p)
+# Test 3
+x = Variable(1)
+p = Problem(:minimize, 2.0 * x, [x >= 2, x <= 4])
+solve!(p)
+@assert abs(p.optval - 4) <= TOLERANCE
 
 # # Test 4
-# x = Variable(2)
-# p = Problem(:minimize, dot([2.0; 2.0], x), [x >= [1.1; 1.1]])
-# solve!(p)
+x = Variable(2)
+p = Problem(:minimize, dot([2.0; 2.0], x), [x >= [1.1; 1.1]])
+solve!(p)
+@assert abs(p.optval - 4.4) <= TOLERANCE
 
-# # Test 5
-# x = Variable(2)
-# A = 1.5 * eye(2)
-# p = Problem(:minimize, dot([2.0; 2.0], x), [A * x >= [1.1; 1.1]])
-# solve!(p)
+
+# Test 5
+x = Variable(2)
+A = 1.5 * eye(2)
+p = Problem(:minimize, dot([2.0; 2.0], x), [A * x >= [1.1; 1.1]])
+solve!(p)
+@assert abs(p.optval - 2.9333) <= TOLERANCE
 
 # # Test 6
-# x = Variable(1)
-# y = Variable(1)
-# p = Problem(:minimize, x + y, [x >= 3, y >= 2])
-# solve!(p)
-
-# # Test 7
-# x = Variable(1)
-# y = Variable(1)
-# p = Problem(:minimize, 2.0*x - 5.0*y, [100.0 <= x, x >= 200.0, 80.0 <= y, y <= 170.0, y >= -x])
-# solve!(p)
-
-# # Test 8
-# x = Variable(2)
-# y = Variable(2)
-# p = Problem(:minimize, dot([2.0; 2.0], x) + dot([2.0; 2.0], y), [x >= [1.1; 1.1], y >= [1.1; 1.1]])
-# solve!(p)
-
-# # Test 9
-# x = Variable(2)
-# p = Problem(:minimize, x[1] + x[2], [x >= 1])
-# solve!(p)
-
-# TODO: take care of this
 x = Variable(1)
 y = Variable(1)
-#p = Problem(:maximize, -2.0*x + 5.0*y, [100.0 <= x, x <= 200.0, 80.0 <= y, y <= 170.0, y >= -x + 200])
-#solve!(p)
+p = Problem(:minimize, x + y, [x >= 3, y >= 2])
+solve!(p)
+@assert abs(p.optval - 5) <= TOLERANCE
 
-# sol = ecos_solve(n=2, m=2, p=0, G=[-1 -2; -2 -1], c=[1.0, 1.0], h=[-2.0, -2.0])
-#TOL = .0001
+# Test 7
+x = Variable(1)
+y = Variable(1)
+p = Problem(:minimize, 2.0*x - 5.0*y, [100.0 <= x, x <= 200.0, 80.0 <= y, y <= 170.0, y >= -x])
+solve!(p)
+@assert abs(p.optval + 650) <= TOLERANCE
 
-#x = Parameter(3)
-#y = Variable(3,4)
-#z = x + 2
-#w = 3*y
-#q = rand(3,3)*w + y
+# Test 8
+x = Variable(2)
+p = Problem(:minimize, x[1] + x[2], [x >= 1])
+solve!(p)
+@assert abs(p.optval - 2) <= TOLERANCE
 
-#y = Variable()
-#x = Variable(3)
-#z = y*[1.0,2.0,3.0]
-#c = [y <= 3.0, y >= 0.0, x >= ones(3), x <= z]
-#o = 3*y
-#p = Problem(:minimize,o,c)
-#solve!(p)
-#@assert abs(p.optval - 3) < TOL
-#println(x.value)
-#@assert x.value - y.value*[1.0,2.0,3.0] <= 0
+y = Variable(1)
+x = Variable(2)
+z = y*[1; 2]
+c = [y >= 0.0, x >= ones(2), x <= z]
+p = Problem(:minimize,y,c)
+# solve!(p)
+
+# y = Variable(1)
+# x = Variable(3)
+# z = y*[1.0,2.0,3.0]
+# c = [y <= 3.0, y >= 0.0, x >= ones(3), x <= z]
+# o = 3*y
+# p = Problem(:minimize,o,c)
+# solve!(p)
+# @assert abs(p.optval - 3) < TOL
+# println(x.value)
+# @assert x.value - y.value*[1.0,2.0,3.0] <= 0
 
 # try some atoms
 #y = Variable()
