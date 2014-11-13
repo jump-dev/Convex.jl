6f88306a3a989b9af6263b5cebcb83efef9d3c73
Karanveer Mohan <karanveer.1992@gmail.com>
2014-11-13 13:55:32 -0800

    add travis

--- test/run_tests.jl
+++ test/run_tests.jl
@@ -1,6 +1,6 @@
 tests = ["test.jl",
-         "test_new.jl",
-         "test_exp.jl",
+         "test2.jl"]
+tests_scs = ["test_exp.jl",
          "test_sdp.jl"]
 
 println("Running tests:")
@@ -8,4 +8,11 @@ println("Running tests:")
 for curtest in tests
     println(" Test: $(curtest)")
     include(curtest)
+end
+
+if isdir(Pkg.dir("SCS"))
+	for curtest in tests_scs
+    println(" Test: $(curtest)")
+    include(curtest)
+	end
 end
