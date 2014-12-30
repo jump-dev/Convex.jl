3374c758a14904cfe7c035351957604006017e1b
Karanveer Mohan <karanveer.1992@gmail.com>
2014-12-31 00:45:31 +0530

    fix conic_form! when symbol is :Bin

--- src/constraints/signs_and_sets.jl
+++ src/constraints/signs_and_sets.jl
@@ -1,8 +1,10 @@
 export conic_form!
 
-conic_form!(s::Positive, x::Variable, unique_conic_forms) = conic_form!(x>=0, unique_conic_forms) 
-conic_form!(s::Negative, x::Variable, unique_conic_forms) = conic_form!(x<=0, unique_conic_forms) 
+conic_form!(s::Positive, x::Variable, unique_conic_forms) = conic_form!(x>=0, unique_conic_forms)
+conic_form!(s::Negative, x::Variable, unique_conic_forms) = conic_form!(x<=0, unique_conic_forms)
 
 function conic_form!(set::Symbol, x::Variable, unique_conic_forms)
-	conic_form!(SDPConstraint(x), unique_conic_forms)
+	if set==:Semidefinite
+		conic_form!(SDPConstraint(x), unique_conic_forms)
+	end
 end
