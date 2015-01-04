0b857c40e6fd34ee069e9cf2e53a36284a7cbc99
Karanveer Mohan <karanveer.1992@gmail.com>
2015-01-04 15:06:37 -0800

    Update conf.py to not import rtd_theme on prod

--- docs/conf.py
+++ docs/conf.py
@@ -260,7 +260,15 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 # Added by Karanveer for the sphinx theme locally: https://github.com/snide/sphinx_rtd_theme
-import sphinx_rtd_theme
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
 
 html_theme = "sphinx_rtd_theme"
 
