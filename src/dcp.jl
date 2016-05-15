72379d2f943173db66e8d40160516ce465713ea3
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2016-05-16 00:11:58 +0530

    Enabled Convex.jl to accept the complex arguments

--- src/dcp.jl
+++ src/dcp.jl
@@ -13,9 +13,7 @@
 import Base.-, Base.+, Base.*
 export Vexity, ConstVexity, AffineVexity, ConvexVexity, ConcaveVexity, NotDcp
 export Monotonicity, Nonincreasing, Nondecreasing, NoMonotonicity
-export Sign, Positive, Negative, NoSign, NotDefined
-# exported Doamin and its subtypes
-export Domain, Real, Complex
+export Sign, Positive, Negative, NoSign, Complex
 export -, +, *
 
 # Vexity subtypes
