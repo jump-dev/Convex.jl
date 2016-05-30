28866d9b801208d60ad3fb9f730e804dd538133d
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2016-05-30 05:41:50 +0530

    Added checks to ensure that objective and inequality constraints are real

--- src/constraints/constraints.jl
+++ src/constraints/constraints.jl
@@ -14,7 +14,7 @@ type EqConstraint <: Constraint
   dual::ValueOrNothing
 
   function EqConstraint(lhs::AbstractExpr, rhs::AbstractExpr)
-    if (sign(lhs) == Convex.ComplexSign() || sign(lhs) == Convex.ComplexSign()) && (sign(lhs)!=sign(rhs))
+    if (sign(lhs) == Convex.ComplexSign() || sign(lhs) == Convex.ComplexSign()) && (sign(lhs) != sign(rhs))
       error("Cannot create equality constraint between expressions of size $(sign(lhs) and $(sign(rhs)")
     else
       if lhs.size == rhs.size || lhs.size == (1, 1)
