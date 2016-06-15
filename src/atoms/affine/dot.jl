60929e422d447b4fb744146c10ae0f5e18e29e6e
Madeleine Udell <madeleine.udell@gmail.com>
2016-06-15 16:51:09 -0700

    fixed dot product for matrices

--- src/atoms/affine/dot.jl
+++ src/atoms/affine/dot.jl
@@ -1,6 +1,6 @@
 import Base.dot
 export dot
 
-dot(x::AbstractExpr, y::AbstractExpr) = x' * y
-dot(x::Value, y::AbstractExpr) = Constant(x') * y
-dot(x::AbstractExpr, y::Value) = x' * Constant(y)
+dot(x::AbstractExpr, y::AbstractExpr) = sum(x .* y)
+dot(x::Value, y::AbstractExpr) = sum(Constant(x) .* y)
+dot(x::AbstractExpr, y::Value) = sum(x .* Constant(y))
