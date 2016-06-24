444ea57bec9542c06ccef79aa5e4fbf4d8496c96
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2016-06-24 07:15:52 -0400

    corrcted real_imag atom

--- src/atoms/affine/real_imag.jl
+++ src/atoms/affine/real_imag.jl
@@ -44,22 +44,23 @@ end
 
 function conic_form!(x::RealAtom, unique_conic_forms::UniqueConicForms)
   if !has_conic_form(unique_conic_forms, x)
+    new_objective = ConicObj()
     objective = conic_form!(x.children[1], unique_conic_forms)
     # Special case for constants
     if x.children[1].head == :constant
       for var in keys(objective)
         re = real(objective[var][1])
         im = zeros(size(re))
-        objective[var] = (re,im)
+        new_objective[var] = (re,im)
       end
     else 
       for var in keys(objective)
         re = real(objective[var][1])
         im = real(objective[var][2])
-        objective[var] = (re,im)
+        new_objective[var] = (re,im)
       end
     end
-    cache_conic_form!(unique_conic_forms, x, objective)
+    cache_conic_form!(unique_conic_forms, x, new_objective)
   end
   return get_conic_form(unique_conic_forms, x)
 end
@@ -101,21 +102,22 @@ end
 
 function conic_form!(x::ImaginaryAtom, unique_conic_forms::UniqueConicForms)
   if !has_conic_form(unique_conic_forms, x)
+    new_objective = ConicObj()
     objective = conic_form!(x.children[1], unique_conic_forms)
     if x.children[1].head == :constant
       for var in keys(objective)
         re = real(objective[var][2])
         im = zeros(size(re))
-        objective[var] = (re, im)
+        new_objective[var] = (re, im)
       end
     else
       for var in keys(objective)
         re = imag(objective[var][1])
         im = imag(objective[var][2])
-        objective[var] = (re,im)
+        new_objective[var] = (re,im)
       end
     end
-    cache_conic_form!(unique_conic_forms, x, objective)
+    cache_conic_form!(unique_conic_forms, x, new_objective)
   end
   return get_conic_form(unique_conic_forms, x)
 end
