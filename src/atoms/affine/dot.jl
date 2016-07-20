e89c533e6a02772efad87963bc68b449c75431fe
Madeleine Udell <madeleine.udell@gmail.com>
2016-07-20 12:11:56 -0400

    fix error handling in dot

--- src/atoms/affine/dot.jl
+++ src/atoms/affine/dot.jl
@@ -5,9 +5,9 @@ vecdot(x::AbstractExpr, y::AbstractExpr) = sum(x .* y)
 vecdot(x::Value, y::AbstractExpr) = sum(Constant(x) .* y)
 vecdot(x::AbstractExpr, y::Value) = sum(x .* Constant(y))
 
-dot(x::AbstractExpr, y::AbstractExpr) = (ismatrix(x) || ismatrix(y)) ? MethodError("dot not implemented for matrices. perhaps you're looking for vecdot?") : vecdot(x, y)
-dot(x::Value, y::AbstractExpr) = (ismatrix(x) || ismatrix(y)) ? MethodError("dot not implemented for matrices. perhaps you're looking for vecdot?") : vecdot(x, y)
-dot(x::AbstractExpr, y::Value) = (ismatrix(x) || ismatrix(y)) ? MethodError("dot not implemented for matrices. perhaps you're looking for vecdot?") : vecdot(x, y)
+dot(x::AbstractExpr, y::AbstractExpr) = (ismatrix(x) || ismatrix(y)) ? throw(error("dot not implemented for matrices. perhaps you're looking for vecdot?")) : vecdot(x, y)
+dot(x::Value, y::AbstractExpr) = (ismatrix(x) || ismatrix(y)) ? throw(error("dot not implemented for matrices. perhaps you're looking for vecdot?")) : vecdot(x, y)
+dot(x::AbstractExpr, y::Value) = (ismatrix(x) || ismatrix(y)) ? throw(error("dot not implemented for matrices. perhaps you're looking for vecdot?")) : vecdot(x, y)
 
 # tests if an array is a matrix (2D array) with both dimensions of size > 1
 function ismatrix(x)
