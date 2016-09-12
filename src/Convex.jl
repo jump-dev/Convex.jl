ba0bd1b283d6b6a8b6f7e4a16108ddc017e0c637
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2016-09-12 20:19:38 +0530

    fixed confict in Convex.jl

--- src/Convex.jl
+++ src/Convex.jl
@@ -32,6 +32,7 @@ include("atoms/affine/dot.jl")
 include("atoms/affine/reshape.jl")
 include("atoms/affine/trace.jl")
 include("atoms/affine/conv.jl")
+include("atoms/affine/real_imag.jl")
 ### lp atoms
 include("atoms/lp_cone/abs.jl")
 include("atoms/lp_cone/maximum.jl")
@@ -40,6 +41,8 @@ include("atoms/lp_cone/max.jl")
 include("atoms/lp_cone/min.jl")
 include("atoms/lp_cone/sumlargest.jl")
 include("atoms/lp_cone/dotsort.jl")
+
+
 
 ### SOC atoms
 include("atoms/second_order_cone/norm.jl") # also includes some lp atoms
