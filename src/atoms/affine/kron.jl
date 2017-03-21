6b5cc82345b1d277d05296ca743749ba09f99f54
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-21 21:34:20 +0530

    Use element wise multiplication instead of multiplication

--- src/atoms/affine/kron.jl
+++ src/atoms/affine/kron.jl
@@ -3,10 +3,11 @@ export kron
 
 function kron(a::Union{Value, Constant}, b::AbstractExpr)
   rows = AbstractExpr[]
+  a = Constant(a)
   for i in 1:size(a)[1]
     row = AbstractExpr[]
     for j in 1:size(a)[2]
-      push!(row, a[i, j] * b)
+      push!(row, a[i, j] .* b)
     end
     push!(rows, foldl(hcat, row))
   end
@@ -16,10 +17,11 @@ end
 
 function kron(a::AbstractExpr, b::Union{Value, Constant})
   rows = AbstractExpr[]
+  b = Constant(b) 
   for i in 1:size(a)[1]
     row = AbstractExpr[]
     for j in 1:size(a)[2]
-      push!(row, a[i, j] * b)
+      push!(row, a[i, j] .* b)
     end
     push!(rows, foldl(hcat, row))
   end
