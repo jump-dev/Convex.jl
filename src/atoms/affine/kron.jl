634e4f4bc590494e8516c184a92edd750d8fb5ea
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-22 21:16:35 +0530

    Modified kron to accept any argument as a variable

--- src/atoms/affine/kron.jl
+++ src/atoms/affine/kron.jl
@@ -1,8 +1,11 @@
 import Base.kron
 export kron
 
+#### TO DO wite the conic_form implemenatatioo
+
 function kron(a::Union{AbstractArray, Convex.Constant}, b::Convex.Variable)
   rows = Convex.AbstractExpr[]
+  a = Constant(a)
   for i in 1:size(a)[1]
     row = Convex.AbstractExpr[]
     for j in 1:size(a)[2]
@@ -12,3 +15,19 @@ function kron(a::Union{AbstractArray, Convex.Constant}, b::Convex.Variable)
   end
   return foldl(vcat, rows)
 end
+
+
+
+function kron(a::Convex.Variable, b::Union{AbstractArray, Convex.Constant})
+  rows = Convex.AbstractExpr[]
+  b = Constant(b)
+  for i in 1:size(a)[1]
+    row = Convex.AbstractExpr[]
+    for j in 1:size(a)[2]
+      push!(row, a[i, j] * b)
+    end
+    push!(rows, foldl(hcat, row))
+  end
+  return foldl(vcat, rows)
+end
+
