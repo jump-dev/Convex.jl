00561561d9d8e88f910aa50955932405785f25a2
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-22 20:15:04 +0530

    Do not Interchange hcat and vcat

--- src/atoms/affine/kron.jl
+++ src/atoms/affine/kron.jl
@@ -52,10 +52,10 @@ function conic_form!(x::KronAtom, unique_conic_forms::UniqueConicForms)
           push!(row1,xx)
           push!(row2,y)
         end
-        push!(rows1, foldl(vcat, row1))
-        push!(rows2, foldl(vcat, row2))
+        push!(rows1, foldl(hcat, row1))
+        push!(rows2, foldl(hcat, row2))
       end
-      objective[key] = (foldl(hcat, rows1),foldl(hcat, rows2))
+      objective[key] = (foldl(vcat, rows1),foldl(vcat, rows2))
     end
 
     cache_conic_form!(unique_conic_forms, x, objective)
