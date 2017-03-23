ea7ca8feb623eff943d212784e33a80360fb90e2
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-23 20:18:05 +0530

    Failed again :(

--- src/atoms/affine/kron.jl
+++ src/atoms/affine/kron.jl
@@ -1,53 +1,32 @@
 import Base.kron
 export kron
-export sign, monotonicity, curvature, evaluate, conic_form!
 
-type KronAtom <: AbstractExpr
-  head::Symbol
-  id_hash::UInt64
-  children::Tuple{AbstractExpr, AbstractExpr}
-  size::Tuple{Int, Int}
 
-  function KronAtom(x::AbstractExpr, y::AbstractExpr)
-    if vexity(x) != ConstVexity() && vexity(y) != ConstVexity()
-      error("Kron of two non-constant expressions is not DCP compliant")
-    else
-      sz = (size(x)[1]*size(y)[1], size(x)[2]*size(y)[2])
-      children = (x, y)
-      return new(:kron, hash(children), children, sz)
+#### TODO: wite the conic_form implemenatatioo
+
+function kron(a::Union{AbstractArray, Convex.Constant}, b::Convex.Variable)
+  rows = Convex.AbstractExpr[]
+  a = Constant(a)
+  for i in 1:size(a)[1]
+    row = Convex.AbstractExpr[]
+    for j in 1:size(a)[2]
+      push!(row, a[i, j] * b)
     end
+    push!(rows, foldl(hcat, row))
   end
-end
-
-function sign(x::KronAtom)
-    return sign(x.children[1]) * sign(x.children[2])
-end
-
-function monotonicity(x::KronAtom)
-  return (sign(x.children[2]) * Nondecreasing(), sign(x.children[1]) * Nondecreasing())
+  return foldl(vcat, rows)
 end
 
 
-function curvature(x::KronAtom)
-    return ConstVexity()
-end
-
-function evaluate(x::KronAtom)
-  return kron(evaluate(x.children[1]),evaluate(x.children[2]))
-end
-
-function conic_form!(x::KronAtom, unique_conic_forms::UniqueConicForms)
-  if !has_conic_form(unique_conic_forms, x)
-    objective = conic_form!(x.children[2], unique_conic_forms)
-    a = evaluate(x.children[1])
-    for key in objective.keys
-      xx = kron(objective[key][1],a)
-      yy = kron(objective[key][2],a)
-      objective[key] = (xx,yy)
+function kron(a::Convex.Variable, b::Union{AbstractArray, Convex.Constant})
+  rows = Convex.AbstractExpr[]
+  b = Constant(b)
+  for i in 1:size(a)[1]
+    row = Convex.AbstractExpr[]
+    for j in 1:size(a)[2]
+      push!(row, a[i, j] * b)
     end
-    cache_conic_form!(unique_conic_forms, x, objective)
+    push!(rows, foldl(hcat, row))
   end
-  return get_conic_form(unique_conic_forms, x)
+  return foldl(vcat, rows)
 end
-
-kron(x::Value, y::AbstractExpr) = KronAtom(Constant(x), y)
