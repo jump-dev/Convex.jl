5b6ede2355c904c5c183b65df2cbabc2efe0555c
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-24 14:52:51 +0530

    replace multiply by elementwise multiplication

--- src/atoms/affine/kron.jl
+++ src/atoms/affine/kron.jl
@@ -1,9 +1,6 @@
 import Base.kron
 export kron
 
-
-#### TODO: wite the conic_form implemenatatioo
-
 function kron(a::Value, b::AbstractExpr)
   rows = AbstractExpr[]
   for i in 1:size(a)[1]
@@ -21,13 +18,12 @@ function kron(a::Value, b::AbstractExpr)
 end
 
 
-function kron(a::Convex.Variable, b::Value)
+function kron(a::AbstractExpr, b::Value)
   rows = AbstractExpr[]
-  #b = Constant(b)
   for i in 1:size(a)[1]
     row = AbstractExpr[]
     for j in 1:size(a)[2]
-      push!(row, a[i, j] * b)
+      push!(row, a[i, j] .* b)
     end
     push!(rows, foldl(hcat, row))
   end
