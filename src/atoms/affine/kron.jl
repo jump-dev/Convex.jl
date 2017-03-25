1c047e57f87cc4dfd193a431064794a7d6915764
Ayush Pandey <ayushpandey.iitkgp@gmail.com>
2017-03-25 21:55:27 +0530

    any argument in kron can be an AbstractExpr

--- src/atoms/affine/kron.jl
+++ src/atoms/affine/kron.jl
@@ -1,15 +1,16 @@
 import Base.kron
 export kron
 
-#### TO DO wite the conic_form implemenatatioo
-
-function kron(a::Union{AbstractArray, Convex.Constant}, b::Convex.Variable)
-  rows = Convex.AbstractExpr[]
-  a = Constant(a)
+function kron(a::Value, b::AbstractExpr)
+  rows = AbstractExpr[]
   for i in 1:size(a)[1]
-    row = Convex.AbstractExpr[]
+    row = AbstractExpr[]
     for j in 1:size(a)[2]
-      push!(row, a[i, j] * b)
+      if isreal(a[i,j])
+        push!(row, real(a[i, j]) * b)
+      else
+        push!(row, a[i, j] * b)
+      end        
     end
     push!(rows, foldl(hcat, row))
   end
@@ -17,17 +18,14 @@ function kron(a::Union{AbstractArray, Convex.Constant}, b::Convex.Variable)
 end
 
 
-
-function kron(a::Convex.Variable, b::Union{AbstractArray, Convex.Constant})
-  rows = Convex.AbstractExpr[]
-  b = Constant(b)
+function kron(a::AbstractExpr, b::Value)
+  rows = AbstractExpr[]
   for i in 1:size(a)[1]
-    row = Convex.AbstractExpr[]
+    row = AbstractExpr[]
     for j in 1:size(a)[2]
-      push!(row, a[i, j] * b)
+      push!(row, a[i, j] .* b)
     end
     push!(rows, foldl(hcat, row))
   end
   return foldl(vcat, rows)
 end
-
