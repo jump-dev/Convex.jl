0880ac4bef995c7c6f8016ea0cfb7bc5424167a3
Mattias Fält <faldt.mattias@gmail.com>
2017-07-19 23:40:01 +0200

    tabs instead of spaces

--- test/test_params.jl
+++ test/test_params.jl
@@ -32,14 +32,14 @@ facts("Fixed and freed variables") do
 	p = minimize(norm(x-a) + gamma*norm(x[1:end-1] - x[2:end]))
 	solve!(p)
 	o1 = p.optval
-  # x should be very close to a
-  @fact o1 --> roughly(0.7*norm(a[1:end-1] - a[2:end]), TOL)
+	# x should be very close to a
+	@fact o1 --> roughly(0.7*norm(a[1:end-1] - a[2:end]), TOL)
 	# increase regularization
 	fix!(gamma, 1.0)
 	solve!(p)
 	o2 = p.optval
-  # x should be very close to mean(a)
-  @fact o2 --> roughly(norm(a-mean(a)), TOL)
+	# x should be very close to mean(a)
+	@fact o2 --> roughly(norm(a-mean(a)), TOL)
 
 	@fact o1 <= o2 --> true
   end
