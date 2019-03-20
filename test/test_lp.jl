92b70890a88ee58a35e3403c08c6e6f253585d06
tomerarnon <tomerarnon@gmail.com>
2019-03-20 09:56:03 -0700

    reverse neg tests

--- test/test_lp.jl
+++ test/test_lp.jl
@@ -88,11 +88,11 @@
 
     @testset "neg atom" begin
         x = Variable(3)
-        p = minimize(1, [x >= -2, x <= -2, neg(x) >= -3])
+        p = minimize(1, [x >= -2, x <= -2, neg(x) <= -3])
         @test vexity(p) == ConvexVexity()
         solve!(p, solver)
         @test p.optval ≈ 1 atol=TOL
-        @test evaluate(sum(neg(x))) ≈ -6 atol=TOL
+        @test evaluate(sum(neg(x))) ≈ 6 atol=TOL
     end
 
     @testset "sumlargest atom" begin
