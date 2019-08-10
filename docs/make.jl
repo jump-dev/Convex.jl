2f90cfa661fba97950b21083386540c9ff721990
Eric <5846501+ericphanson@users.noreply.github.com>
2019-08-10 01:07:50 +0100

    Fix indentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,28 +1,25 @@
 using Documenter, Convex
 
 makedocs(;
-            modules = [Convex],
-            format=Documenter.HTML(),
-            pages = [
-                "Home" => "index.md",
-                "Installation" => "installation.md",
-                "Quick Tutorial" => "quick_tutorial.md",
-                "Basic Types" => "types.md",
-                "Supported Operations" => "operations.md",
-                "Examples" => "examples.md",
-                "Complex-domain Optimization" => "complex-domain_optimization.md",
-                "Solvers" => "solvers.md",
-                "FAQ" => "faq.md",
-                "Optimizing in a Loop" => "loop.md",
-                "Advanced" => "advanced.md",
-                "Contributing" => "contributing.md",
-                "Credits" => "credits.md"
-            ],
-            repo="https://github.com/JuliaOpt/Convex.jl/blob/{commit}{path}#L{line}",
-            sitename="Convex.jl")
+    modules = [Convex],
+    format = Documenter.HTML(),
+    pages = [
+        "Home" => "index.md",
+        "Installation" => "installation.md",
+        "Quick Tutorial" => "quick_tutorial.md",
+        "Basic Types" => "types.md",
+        "Supported Operations" => "operations.md",
+        "Examples" => "examples.md",
+        "Complex-domain Optimization" => "complex-domain_optimization.md",
+        "Solvers" => "solvers.md",
+        "FAQ" => "faq.md",
+        "Optimizing in a Loop" => "loop.md",
+        "Advanced" => "advanced.md",
+        "Contributing" => "contributing.md",
+        "Credits" => "credits.md"
+    ],
+    repo = "https://github.com/JuliaOpt/Convex.jl/blob/{commit}{path}#L{line}",
+    sitename = "Convex.jl")
 
 
-deploydocs(;
-    repo="https://github.com/JuliaOpt/Convex.jl",
-)
-
+deploydocs(repo = "https://github.com/JuliaOpt/Convex.jl")
