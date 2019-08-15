c8aadeb176d44aa37d8f68cbc1aefec2e02bfb33
Joe Renes <joerenes@gmail.com>
2019-08-15 21:52:17 +0200

    draft of partial transpose with proper conic form (borrowing from Eric Hanson)

--- src/atoms/affine/partialtranspose.jl
+++ src/atoms/affine/partialtranspose.jl
@@ -2,7 +2,7 @@ import Base.sign
 export partialtranspose
 export sign, curvature, monotonicity, evaluate
 
-type PartialTransposeAtom <: AbstractExpr
+struct PartialTransposeAtom <: AbstractExpr
     head::Symbol
     id_hash::UInt64
     children::Tuple{AbstractExpr}
@@ -15,23 +15,22 @@ type PartialTransposeAtom <: AbstractExpr
             error("Only square matrices are supported")
         end
         if ! (1 ≤ sys ≤ length(dims))
-            error("Invalid system, should between 1 and ", length(dims), " got ", sys)
+            error("Invalid system, should between 1 and ", length(dims), "; got ", sys)
         end
         if x.size[1] ≠ prod(dims)
             error("Dimension of system doesn't correspond to dimension of subsystems")
         end
         children = (x, )
-        #newsize = (round(Int, x.size[2]/dims[sys]), round(Int, x.size[1]/dims[sys]))
-        return new(:partialtrace, hash(children), children, x.size, sys, dims)
+        return new(:partialtranspose, hash(children), children, x.size, sys, dims)    
     end
 end
 
 function sign(x::PartialTransposeAtom)
-     return sign(x.children[1])
- end
+    return sign(x.children[1])
+end
 
 function curvature(x::PartialTransposeAtom)
-  return ConstVexity()
+    return ConstVexity()
 end
 
 function monotonicity(x::PartialTransposeAtom)
@@ -39,79 +38,54 @@ function monotonicity(x::PartialTransposeAtom)
 end
 
 function evaluate(x::PartialTransposeAtom)
-    ρ = evaluate(x.children[1])
-    dims = x.dims
-
-    subsystem = function(sys)
-        function term(ρ, j::Int)
-            a = speye(1)
-            b = speye(1)
-            i_sys = 1
-            for dim in dims
-                if i_sys == sys
-                # create a vector that is only 1 at its jth component
-                v = spzeros(dim, 1);
-                v[j] = 1;
-                a = kron(a, v')
-                b = kron(b, v)
-                else
-                    a = kron(a, speye(dim))
-                    b = kron(b, speye(dim))
-                end
-                i_sys += 1
-            end
-            return a * ρ * b
-        end
-        return sum([term(ρ, j) for j in 1:dims[sys]])
-    end
-    sub_systems = [subsystem(i) for i in 1:length(dims)]
-    a = eye(1)
-    for i in 1:length(dims)
-        if i == x.sys
-            a = kron(a,transpose(sub_systems[i]))
-        else
-            a = kron(a,sub_systems[i])
-        end
-    end
-    return a
+    return partialtranspose( evaluate( x.children[1] ))
 end
 
 
-function conic_form!(x::PartialTransposeAtom, unique_conic_forms::UniqueConicForms)
+function partialtranspose(x::AbstractMatrix, sys::Int, dims::Vector) 
+    n = length(dims)
+    d = prod(dims)
+    s = n - sys + 1
+    p = collect(1:2n)
+    p[s] = n + s
+    p[n + s] = s
+
+    rdims = reverse(dims)
+    r = reshape(x, [rdims;rdims]...)
+    return reshape(permutedims(r,p),(d,d))
+end
+
+
+function permutedims_matrix(dims, p)
+    d = prod(dims)
+    n = length(dims)
+    sparse(reshape(PermutedDimsArray(reshape(Matrix(I,d,d), (dims..., dims...)), (p..., (n + 1:2n)...)), (d, d)))
+end
+    
+# borrowing this from transpose.jl: 
+# Since everything is vectorized, we simply need to multiply x by a permutation
+# matrix such that coeff * vectorized(x) - vectorized(x') = 0
+function conic_form!(x::PartialTransposeAtom, unique_conic_forms::UniqueConicForms=UniqueConicForms())
     if !has_conic_form(unique_conic_forms, x)
-        sys = x.sys
-        dims = x.dims
+        objective = conic_form!(x.children[1], unique_conic_forms)
 
-        # compute the partial trace of x by summing over
-        # (I ⊗ <j| ⊗ I) x (I ⊗ |j> ⊗ I) for all j's
-        # in the system we want to trace out
-        # This function returns every term in the sum
-        function term(ρ, j::Int)
-            a = speye(1)
-            b = speye(1)
-            i_sys = 1
-            for dim in dims
-                if i_sys == sys
-                    # create a vector that is only 1 at its jth component
-                    v = spzeros(dim, 1);
-                    v[j] = 1;
-                    a = kron(a, v')
-                    b = kron(b, v)
-                else
-                    a = kron(a, speye(dim))
-                    b = kron(b, speye(dim))
-                end
-                i_sys += 1
-            end
-            return a * ρ * b
-        end
+        n = length(x.dims)
+        d = prod(x.dims)
+        s = n - x.sys + 1
+        p = collect(1:2n)
+        p[s] = n + s
+        p[n + s] = s
 
-        # sum all terms described above for all j's
-        objective = conic_form!(transpose(sum([term(x.children[1], j) for j in 1:dims[sys]])), unique_conic_forms)
+        rdims = reverse(x.dims)
+
+        partialtranspose_matrix = permutedims_matrix([rdims;rdims],p)
+
+        objective = partialtranspose_matrix * objective
         cache_conic_form!(unique_conic_forms, x, objective)
     end
     return get_conic_form(unique_conic_forms, x)
 end
 
-partialtranspose(x::AbstractExpr, sys::Int, dim::Vector) = PartialTransposeAtom(x, sys, dim) 
+partialtranspose(x::AbstractExpr,sys::Int, dim::Vector) = PartialTransposeAtom(x,sys,dim)
+
 
