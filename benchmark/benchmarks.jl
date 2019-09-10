4146dc3d201ca387e9a281f9ab9bf90c677d570d
Eric <5846501+ericphanson@users.noreply.github.com>
2019-09-10 18:46:58 -0400

    Benchmark only 10 problems

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -13,6 +13,6 @@ using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
 
-problems =  Regex[r"constant_fix!_with_complex_numbers", r"affine_dot_multiply_atom", r"affine_hcat_atom",  r"affine_trace_atom", r"exp_entropy_atom", r"exp_log_perspective_atom", r"socp_norm_2_atom", r"socp_quad_form_atom", r"socp_sum_squares_atom", r"lp_norm_inf_atom", r"lp_maximum_atom", r"sdp_and_exp_log_det_atom", r"sdp_norm2_atom", r"sdp_lambda_min_atom", r"sdp_sum_largest_eigs", r"mip_integer_variables"]
+problems =  Regex[r"affine_dot_multiply_atom", r"affine_hcat_atom", r"exp_entropy_atom", r"socp_quad_form_atom", r"socp_sum_squares_atom", r"lp_norm_inf_atom", r"lp_maximum_atom", r"sdp_norm2_atom", r"sdp_lambda_min_atom", r"mip_integer_variables"]
 
 SUITE["formulation"] = ProblemDepot.benchmark_suite(Convex.conic_problem; include=problems)
