ae94ec44c3ed5cd3be77d4f4712e9ab271479380
Eric <5846501+ericphanson@users.noreply.github.com>
2019-11-04 01:57:44 +0000

    Deploy examples to docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,55 @@
-using Documenter, Convex
+using Documenter, Convex, Literate, Pkg
 
-@info "Building example notebooks"
-# build examples notebooks
-notebooks_path = joinpath(@__DIR__, "src", "notebooks")
-include(joinpath(@__DIR__, "..", "examples", "build.jl"))
+@info "Building examples"
 
-@info "Generating `examples.md`"
-# generate the `examples.md` page with links to the notebooks
-filename_to_name(str) = uppercasefirst(replace(replace(splitext(str)[1], "-" => " "), "_" => " "))
-open(joinpath(@__DIR__, "src", "examples.md"), "w") do io
-    println(io, "# Examples")
-    for example_dir in readdir(notebooks_path)
-        name = filename_to_name(example_dir)
-        println(io, "\n## $name\n")
-        for notebook in readdir(joinpath(notebooks_path, example_dir))
-            endswith(notebook, ".ipynb") || continue
-            path = joinpath("notebooks", example_dir, notebook)
-            name = string(filename_to_name(notebook), " (Jupyter notebook)")
-            println(io, "* [$name]($path)")
+filename(str) = first(splitext(last(splitdir(str))))
+filename_to_name(str) = uppercasefirst(replace(replace(filename(str), "-" => " "), "_" => " "))
+
+literate_path = joinpath(@__DIR__(), "..", "examples", "literate")
+build_path =  joinpath(@__DIR__, "src", "examples")
+rm(build_path; force=true, recursive=true)
+isdir(build_path) || mkdir(build_path)
+for dir in readdir(literate_path)
+    dir_path = joinpath(literate_path, dir)
+    isdir(dir_path) || continue
+    @info "Processing directory $dir"
+    build_dir = joinpath(build_path, dir)
+    isdir(build_dir) || mkdir(build_dir)
+    for file in readdir(dir_path)
+        file_path = joinpath(dir_path, file)
+            out_path = joinpath(build_path, dir, file)
+        if endswith(file, ".jl")
+            postprocess = function(content)
+                """
+                ```@setup $(filename(file_path))
+                PREVIOUS_PROJECT = Base.active_project();
+                using Pkg
+                Pkg.activate(@__DIR__)
+                Pkg.instantiate()
+                ```
+                """ * content * """
+                ```@setup $(filename(file_path))
+                Pkg.activate(PREVIOUS_PROJECT)
+                ```
+                """
+            end
+            documenter = true
+            Literate.markdown(file_path, build_dir; documenter = documenter, postprocess =  documenter ? postprocess : identity)
+            Literate.notebook(file_path, build_dir; execute=false)
+            name = string(filename_to_name(file), " (webpage)")
+            path = filename(file) * ".md"
+        else
+            cp(file_path, out_path)
         end
     end
 end
+
+
+function nav_dir(path)
+    [ joinpath(splitpath(path)[end-1:end]..., file) for file in readdir(path) if endswith(file, ".md") && file != "index.md" ]
+end
+
+examples_nav = [ filename_to_name(dir) => nav_dir(joinpath(build_path, dir)) for dir in readdir(build_path) if isdir(joinpath(build_path, dir)) ]
 
 @info "Starting `makedocs`"
 
@@ -33,17 +62,18 @@ makedocs(;
         "Quick Tutorial" => "quick_tutorial.md",
         "Basic Types" => "types.md",
         "Supported Operations" => "operations.md",
-        "Examples" => "examples.md",
         "Complex-domain Optimization" => "complex-domain_optimization.md",
         "Solvers" => "solvers.md",
         "FAQ" => "faq.md",
         "Advanced" => "advanced.md",
         "Problem Depot" => "problem_depot.md",
         "Contributing" => "contributing.md",
-        "Credits" => "credits.md"
+        "Credits" => "credits.md",
+        "Examples" => examples_nav,
     ],
     repo = "https://github.com/JuliaOpt/Convex.jl/blob/{commit}{path}#L{line}",
     sitename = "Convex.jl")
 
+Pkg.activate(@__DIR__)
 
 deploydocs(repo = "github.com/JuliaOpt/Convex.jl.git")
