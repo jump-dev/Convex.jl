aa8668a37aa9a930935b77caa22598b737625c43
Eric <5846501+ericphanson@users.noreply.github.com>
2019-11-07 01:32:46 +0000

    Serve notebooks

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,10 @@
 using Documenter, Convex, Literate, Pkg
 
+# Needed to run GR headless on Travis
 previous_GKSwstype = get(ENV, "GKSwstype", "")
 ENV["GKSwstype"] = "100"
 
-@info "Building examples"
+@info "Building examples..."
 
 filename(str) = first(splitext(last(splitdir(str))))
 filename_to_name(str) = uppercasefirst(replace(replace(filename(str), "-" => " "), "_" => " "))
@@ -12,11 +13,40 @@ function fix_math_md(content)
     replace(content, r"\$\$(.*?)\$\$"s => s"```math\1```")
 end
 
-
 literate_path = joinpath(@__DIR__(), "..", "examples", "literate")
 build_path =  joinpath(@__DIR__, "src", "examples")
 rm(build_path; force=true, recursive=true)
 isdir(build_path) || mkdir(build_path)
+
+
+@info "[Examples] Preparing notebooks..."
+
+notebooks_path = joinpath(@__DIR__, "notebooks")
+rm(notebooks_path, recursive=true, force=true)
+mkdir(notebooks_path)
+
+for dir in readdir(literate_path)
+    dir_path = joinpath(literate_path, dir)
+    isdir(dir_path) || continue
+    @info "Processing directory $dir"
+    notebook_dir = joinpath(notebooks_path, dir)
+    isdir(notebook_dir) || mkdir(notebook_dir)
+    for file in readdir(dir_path)
+        file_path = joinpath(dir_path, file)
+            out_path = joinpath(notebooks_path, dir, file)
+        if endswith(file, ".jl")
+            Literate.notebook(file_path, notebook_dir, execute=false)
+        else
+            cp(file_path, out_path)
+        end
+    end
+end
+
+zip_path = joinpath(build_path, "notebooks.zip")
+run(Cmd(`zip $zip_path -r notebooks`; dir = @__DIR__))
+
+@info "[Examples] Preparing markdown files..."
+
 for dir in readdir(literate_path)
     dir_path = joinpath(literate_path, dir)
     isdir(dir_path) || continue
@@ -35,17 +65,14 @@ for dir in readdir(literate_path)
                 Pkg.activate(@__DIR__)
                 Pkg.instantiate()
                 ```
+                All of the examples can be found in Jupyter notebook form [here](../$(filename(zip_path)).zip).
                 """ * content * """
                 ```@setup $(filename(file_path))
                 Pkg.activate(PREVIOUS_PROJECT)
                 ```
                 """
             end
-            documenter = true
-            Literate.markdown(file_path, build_dir; preprocess = fix_math_md, documenter = documenter, postprocess =  documenter ? postprocess : identity)
-            Literate.notebook(file_path, build_dir; execute=false)
-            name = string(filename_to_name(file), " (webpage)")
-            path = filename(file) * ".md"
+            Literate.markdown(file_path, build_dir; preprocess = fix_math_md, documenter = true, postprocess =  postprocess)
         else
             cp(file_path, out_path)
         end
@@ -53,13 +80,18 @@ for dir in readdir(literate_path)
 end
 
 
+
+@info "Starting `makedocs`"
+
+# Build nav tree for examples
 function nav_dir(dir, path)
     sort([ joinpath("examples", dir, file) for file in readdir(path) if endswith(file, ".md") && file != "index.md" ])
 end
 
 examples_nav = [ filename_to_name(dir) => nav_dir(dir, joinpath(build_path, dir)) for dir in readdir(build_path) if isdir(joinpath(build_path, dir)) ]
 
-@info "Starting `makedocs`"
+# Get current environment, since our examples will change environments
+current_project = Base.active_project()
 
 makedocs(;
     modules = [Convex],
@@ -82,8 +114,10 @@ makedocs(;
     repo = "https://github.com/JuliaOpt/Convex.jl/blob/{commit}{path}#L{line}",
     sitename = "Convex.jl")
 
-Pkg.activate(@__DIR__)
+# Restore environment
+Pkg.activate(current_project)
 
 deploydocs(repo = "github.com/JuliaOpt/Convex.jl.git")
+
 
 ENV["GKSwstype"] = previous_GKSwstype
