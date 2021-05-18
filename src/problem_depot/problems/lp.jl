4971f849642cff0be5b4d7b5a33dfcc0c40b9a80
Nicholas Moehle <nicholasmoehle@gmail.com>
2021-05-18 15:02:39 -0400

    fix the tests for lp_dual_abs_atom (#439)

--- src/problem_depot/problems/lp.jl
+++ src/problem_depot/problems/lp.jl
@@ -26,7 +26,10 @@
         @test p.constraints[2].dual ≈ 1 atol=atol rtol=rtol
         @test p.constraints[3].dual[1,1] ≈ 0 atol=atol rtol=rtol
         @test p.constraints[3].dual[2,2] ≈ 0 atol=atol rtol=rtol
-        @test p.constraints[3].dual[1,2] ≈ p.constraints[3].dual[2,1] atol=atol rtol=rtol
+        @test p.constraints[3].dual[1,2] <= 1 + atol
+        @test p.constraints[3].dual[2,1] <= 1 + atol
+        @test p.constraints[3].dual[1,2] >= -atol
+        @test p.constraints[3].dual[2,1] >= -atol
     end
 end
 
