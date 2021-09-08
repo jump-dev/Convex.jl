0c7d23dd2993a590f989a0bf1a890a393afa7f8b
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-09-08 11:11:40 +0200

    add `assume_psd` option to `quadform`

--- src/atoms/second_order_cone/quadform.jl
+++ src/atoms/second_order_cone/quadform.jl
@@ -1,33 +1,47 @@
-function quadform(x::Value, A::AbstractExpr)
+function quadform(x::Value, A::AbstractExpr; assume_psd=false)
     return x' * A * x
 end
 
 is_psd(A::AbstractMatrix{T}) where {T} = isposdef(A + sqrt(eps(float(real(T))))*Matrix{T}(I, size(A)...))
 
-function quadform(x::AbstractExpr, A::Value)
+function quadform(x::AbstractExpr, A::Value; assume_psd=false)
     if length(size(A)) != 2 || size(A, 1) != size(A, 2)
         error("Quadratic form only takes square matrices")
     end
     if !ishermitian(A)
         error("Quadratic form only defined for Hermitian matrices")
     end
-    if is_psd(A)
+    if assume_psd
         factor = 1
-    elseif is_psd(-A)
-        factor = -1
     else
-        error("Quadratic forms supported only for semidefinite matrices")
+        if is_psd(A)
+            factor = 1
+        elseif is_psd(-A)
+            factor = -1
+        else
+            error("Quadratic forms supported only for semidefinite matrices")
+        end
     end
 
     P = sqrt(Hermitian(factor * A))
     return factor * square(norm2(P * x))
 end
 
-function quadform(x::AbstractExpr, A::AbstractExpr)
+"""
+    quadform(x::AbstractExpr, A::AbstractExpr; assume_psd=false)
+
+Represents `x' * A * x` where either:
+
+* `x` is a vector-valued variable and `A` is a positive semidefinite or negative semidefinite matrix (and in particular Hermitian or real symmetric). If `assume_psd=true`, then
+`A` will be assumed to be positive semidefinite. Otherwise, `Convex.is_psd` will be used to check if `A` is positive semidefinite or negative semidefinite.
+* or `A` is a matrix-valued variable and `x` is a vector.
+
+"""
+function quadform(x::AbstractExpr, A::AbstractExpr; assume_psd=false)
     if vexity(x) == ConstVexity()
-        return quadform(evaluate(x), A)
+        return quadform(evaluate(x), A; assume_psd=assume_psd)
     elseif vexity(A) == ConstVexity()
-        return quadform(x, evaluate(A))
+        return quadform(x, evaluate(A); assume_psd=assume_psd)
     else
         error("Either `x` or `A` must be constant in `quadform(x,A)`.")
     end
