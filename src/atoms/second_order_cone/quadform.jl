35b8d6b0d47551f31181cba866d68a606a235820
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-09-15 00:10:20 +0200

    add `assume_psd` option to `quadform` (#456)
    * add `assume_psd` option to `quadform`
    
    * Use sparse symmetric factorization when possible (#457)
    
    * Use sparse symmetric factorization when possible
    
    * Address review comments
    
    * Test PSD-ness of sparse input
    
    * Fix imports and type conflicts
    
    Co-authored-by: mtanneau <9593025+mtanneau@users.noreply.github.com>

--- src/atoms/second_order_cone/quadform.jl
+++ src/atoms/second_order_cone/quadform.jl
@@ -1,33 +1,77 @@
-function quadform(x::Value, A::AbstractExpr)
+function quadform(x::Value, A::AbstractExpr; assume_psd=false)
     return x' * A * x
 end
 
-is_psd(A::AbstractMatrix{T}) where {T} = isposdef(A + sqrt(eps(float(real(T))))*Matrix{T}(I, size(A)...))
+"""
+    is_psd(A; tol)
 
-function quadform(x::AbstractExpr, A::Value)
+Check whether `A` is positive semi-definite by computing a LDLᵀ factorization of `A + tol*I`
+"""
+function is_psd(A; tol=sqrt(eps(float(real(eltype(A))))))
+    T = eltype(A)
+    # If `A` is neither a Matrix nor SparseMatrixCSC, we do the following:
+    # * sparse fallback if the arithmetic is supported
+    # * dense fallack otherwise
+    if T <: AbstractFloat || T <: LinearAlgebra.BlasFloat
+        return is_psd(sparse(A); tol=tol)
+    else
+        return is_psd(Matrix(A); tol=tol)
+    end
+end
+is_psd(A::SparseMatrixCSC{Complex{T}}; tol::T=sqrt(eps(T))) where{T<:LinearAlgebra.BlasReal} = isposdef(A + tol*I)
+function is_psd(A::SparseMatrixCSC{T}; tol::T=sqrt(eps(T))) where{T<:AbstractFloat}
+    # LDLFactorizations requires the input matrix to only have the upper triangle.
+    A_ = Symmetric(sparse(UpperTriangular(A)) + tol*I)
+    try
+        F = ldl(A_)
+        d = F.D.diag
+        return minimum(d) >= 0
+    catch err
+        # If the matrix could not be factorized, then it is not PSD
+        isa(err, LDLFactorizations.SQDException) && return false
+        rethrow()  # Something else happened
+    end
+end
+is_psd(A::Matrix; tol=sqrt(eps(float(real(eltype(A)))))) = isposdef(A + tol*I)
+
+function quadform(x::AbstractExpr, A::Value; assume_psd=false)
     if length(size(A)) != 2 || size(A, 1) != size(A, 2)
         error("Quadratic form only takes square matrices")
     end
     if !ishermitian(A)
         error("Quadratic form only defined for Hermitian matrices")
     end
-    if is_psd(A)
+    if assume_psd
         factor = 1
-    elseif is_psd(-A)
-        factor = -1
     else
-        error("Quadratic forms supported only for semidefinite matrices")
+        if is_psd(A)
+            factor = 1
+        elseif is_psd(-A)
+            factor = -1
+        else
+            error("Quadratic forms supported only for semidefinite matrices")
+        end
     end
 
     P = sqrt(Hermitian(factor * A))
     return factor * square(norm2(P * x))
 end
 
-function quadform(x::AbstractExpr, A::AbstractExpr)
+"""
+    quadform(x::AbstractExpr, A::AbstractExpr; assume_psd=false)
+
+Represents `x' * A * x` where either:
+
+* `x` is a vector-valued variable and `A` is a positive semidefinite or negative semidefinite matrix (and in particular Hermitian or real symmetric). If `assume_psd=true`, then
+`A` will be assumed to be positive semidefinite. Otherwise, `Convex.is_psd` will be used to check if `A` is positive semidefinite or negative semidefinite.
+* or `A` is a matrix-valued variable and `x` is a vector.
+
+"""
+function quadform(x::AbstractExpr, A::AbstractExpr; assume_psd=false)
     if vexity(x) == ConstVexity()
-        return quadform(evaluate(x), A)
+        return quadform(evaluate(x), A; assume_psd=assume_psd)
     elseif vexity(A) == ConstVexity()
-        return quadform(x, evaluate(A))
+        return quadform(x, evaluate(A); assume_psd=assume_psd)
     else
         error("Either `x` or `A` must be constant in `quadform(x,A)`.")
     end
