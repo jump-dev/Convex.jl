0e421fa7c8a42450bbc777387c6b3c3a775baa19
odow <o.dowson@gmail.com>
2022-02-23 14:52:00 +1300

    Updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,21 +35,14 @@ end
     include("test_abstract_variable.jl")
 
     @testset "SCS with warmstarts" begin
-        # "sdp_lieb_ando" is currently (14 Nov 2021) failing with SCS
-        # on ubuntu in CI (they pass locally on MacOS and in CI with
-        # MacOS, and have passed on ubuntu in the past). Disabling
-        # them for now; once COSMO or Hypatia is on MOI v0.10, we can
-        # try using them, or hope SCS starts solving them again.
-        #
-        # "sdp_sdp_constraints" is failing with MOI v0.9 on ubuntu.
-        run_tests(; exclude=[r"mip", r"sdp_lieb_ando", r"sdp_sdp_constraints"]) do p
-            solve!(p, MOI.OptimizerWithAttributes(SCS.Optimizer, "verbose" => 0, "eps_abs" => 1e-6); warmstart = true)
+        run_tests(; exclude=[r"mip"]) do p
+            solve!(p, MOI.OptimizerWithAttributes(SCS.Optimizer, "verbose" => 0, "eps_rel" => 1e-6, "eps_abs" => 1e-6); warmstart = true)
         end
     end
 
     @testset "SCS" begin
-        run_tests(; exclude=[r"mip", r"sdp_lieb_ando", r"sdp_sdp_constraints"]) do p
-            solve!(p, MOI.OptimizerWithAttributes(SCS.Optimizer, "verbose" => 0, "eps_abs" => 1e-6))
+        run_tests(; exclude=[r"mip"]) do p
+            solve!(p, MOI.OptimizerWithAttributes(SCS.Optimizer, "verbose" => 0, "eps_rel" => 1e-6, "eps_abs" => 1e-6))
         end
     end
 
