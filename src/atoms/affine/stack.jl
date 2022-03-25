6d6cbb4d5cdb0c0b2170537ce9eb4c60dfef6c61
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-03-25 14:40:03 +0100

    Change Dict -> OrderedDict
    closes #488

--- src/atoms/affine/stack.jl
+++ src/atoms/affine/stack.jl
@@ -45,7 +45,7 @@ function conic_form!(x::HcatAtom, unique_conic_forms::UniqueConicForms)
             push!(objectives, conic_form!(child, unique_conic_forms))
         end
         # build a dict from variable ids to sizes
-        variable_to_sizes = Dict{UInt64,Int}()
+        variable_to_sizes = OrderedDict{UInt64,Int}()
         for objective in objectives
             for id in keys(objective)
                 if !(id in keys(variable_to_sizes))
