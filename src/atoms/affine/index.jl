575dfc21e9234f766b66a2e409b983818a8bd411
Theo Diamandis <tjdiamandis@gmail.com>
2023-02-06 19:59:45 -0800

    N dimensional

--- src/atoms/affine/index.jl
+++ src/atoms/affine/index.jl
@@ -134,6 +134,6 @@ getindex(x::AbstractExpr, cln_r::Colon, col) = getindex(x, 1:size(x)[1], col)
 getindex(x::AbstractExpr, row, cln_c::Colon) = getindex(x, row, 1:size(x)[2])
 
 # Cartesian Index
-Convex.getindex(x::AbstractExpr, c::CartesianIndex{2}) = x[c[1], c[2]]
+getindex(x::AbstractExpr, c::CartesianIndex{N}) where {N} = x[Tuple(c)...]
 
 ## API Definition ends
