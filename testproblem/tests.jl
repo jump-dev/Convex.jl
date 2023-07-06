74be9b4293837cdf045e70cd8cae998a43b8e894
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-07-07 01:13:42 +0200

    add tests

--- testproblem/tests.jl
+++ testproblem/tests.jl
@@ -5,6 +5,31 @@ Convex.USE_SPARSE() = false
 Convex.USE_SPARSE2() = true
 Convex.USE_SPARSE3() = false
 
+@testset "SCS without duals" begin
+
+run_tests(
+    exclude = [
+        r"mip",
+        # TODO(odow): investigate
+        r"sdp_lieb_ando",
+        # Tolerance issue with SCS 3.0
+        r"sdp_Real_Variables_with_complex_equality_constraints",
+        r"dual"
+    ],
+) do p
+    return solve2!(
+        p,
+        MOI.OptimizerWithAttributes(
+            SCS.Optimizer,
+            "verbose" => 0,
+            "eps_rel" => 1e-6,
+            "eps_abs" => 1e-6,
+        );
+    )
+end
+
+end
+
 @testset "SCS" begin
     run_tests([r"dual"], exclude = [r"socp"]) do p
         return solve2!(
