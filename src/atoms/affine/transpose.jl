c4c32ea39344fec88a8ae0f1630f592da4192e3d
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-07-08 23:59:40 +0200

    fix strange bug in `transpose`

--- src/atoms/affine/transpose.jl
+++ src/atoms/affine/transpose.jl
@@ -1,23 +1,7 @@
+# Since everything is vectorized, we simply need to multiply x by a permutation
+# matrix such that coeff * vectorized(x) - vectorized(transpose(x)) = 0
 function LinearAlgebra.transpose(x::AbstractExpr)
-    sz = length(x)
-
-    num_rows = x.size[1]
-    num_cols = x.size[2]
-
-    I = Array{Int}(undef, sz)
-    J = Array{Int}(undef, sz)
-
-    k = 1
-    for r in 1:num_rows
-        for c in 1:num_cols
-            I[k] = (c - 1) * num_rows + r
-            J[k] = (r - 1) * num_cols + c
-            k += 1
-        end
-    end
-
-    transpose_matrix = sparse(I, J, 1.0)
-
+    transpose_matrix = permutedims_matrix(size(x), (2, 1))
     return reshape(transpose_matrix * vec(x), size(x, 2), size(x, 1))
 end
 
