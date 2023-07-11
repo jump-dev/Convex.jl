55d55eba19e3b8e8e46d373733a1d90cad0b36a3
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-07-12 01:14:39 +0200

    Restore `norm` from master

--- src/atoms/second_order_cone/norm.jl
+++ src/atoms/second_order_cone/norm.jl
@@ -5,13 +5,11 @@ norm_inf(x::AbstractExpr) = maximum(abs(x))
 norm_1(x::AbstractExpr) = sum(abs(x))
 norm_fro(x::AbstractExpr) = norm2(vec(x))
 
-# behavior of norm should be consistent with julia:
-# * vector norms for vectors
-# * operator norms for matrices
 """
     norm(x::AbstractExpr, p::Real=2)
 
 Computes the `p`-norm `‖x‖ₚ = (∑ᵢ |xᵢ|^p)^(1/p)` of a vector expression `x`.
+
 This function uses specialized methods for `p=1, 2, Inf`. For `p > 1` otherwise,
 this function uses the procedure documented at
 [`rational_to_socp.pdf`](https://github.com/jump-dev/Convex.jl/raw/master/docs/supplementary/rational_to_socp.pdf),
@@ -25,7 +23,7 @@ Mathematical Programming, Series B, 95:3-51, 2001.
     `norm(x, p)` will return `‖vec(x)‖ₚ`, matching the behavior of [`norm`](@ref)
     for numeric matrices.
 """
-function norm(x::AbstractExpr, p::Real = 2)
+function LinearAlgebra.norm(x::AbstractExpr, p::Real = 2)
     if length(size(x)) <= 1 || minimum(size(x)) == 1
         # x is a vector
         if p == 1
@@ -41,19 +39,8 @@ function norm(x::AbstractExpr, p::Real = 2)
             error("vector p-norms not defined for p < 1")
         end
     else
-        # TODO: After the deprecation period, allow this again but make it consistent with
-        # LinearAlgebra, i.e. make norm(x, p) for x a matrix the same as norm(vec(x), p).
-        Base.depwarn(
-            "`norm(x, p)` for matrices will in the future be equivalent to " *
-            "`norm(vec(x), p)`. Use `opnorm(x, p)` for the Julia 0.6 behavior of " *
-            "computing the operator norm for matrices.",
-            :norm,
+        error(
+            "In Convex.jl v0.13 and below, `norm(x, p)` meant `opnorm(x, p)` (but was deprecated since v0.8.0). In the future, `norm(x,p)` for matrices will be equivalent to `norm(vec(x),p)`. This is currently an error to ensure you update your code!",
         )
-        return opnorm(x, p)
     end
 end
-
-if isdefined(LinearAlgebra, :vecnorm) # deprecated but defined
-    import LinearAlgebra: vecnorm
-end
-Base.@deprecate vecnorm(x::AbstractExpr, p::Real = 2) norm(vec(x), p)
