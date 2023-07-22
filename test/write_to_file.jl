a14b767e68534e8b89722c2851aa6a20759d2432
Max Kapur <maxkapur@gmail.com>
2023-07-22 19:35:12 -0400

    correct list of formats in unit test, and condition output on compatibility with the problem

--- test/write_to_file.jl
+++ test/write_to_file.jl
@@ -1,9 +1,7 @@
 # Tests of the write_to_file(problem, filename) interface
 
 # Simple quadratic program
-# Can be written in any of the formats
-let
-    moi_file_formats = ["cbf", "lp", "mof", "mps", "nl", "rew", "sdpa"]
+@testset "Simply quadratic program" begin
     filename, _io = Base.Filesystem.mktemp()
 
     m, n = 5, 10
@@ -12,30 +10,48 @@ let
     b = randn(m)
     problem = minimize(sumsquares(A * x - b), [0 <= x, x <= 1])
 
-    # Haven't solved the problem yet
+    # Haven't solved the problem yet, should get an ArgumentError
     @test_throws ArgumentError write_to_file(problem, filename * ".sdpa")
 
-    # Make sure files are written without error
-    # Can't check correctness since we don't provide a read_to_file function
     solve!(problem, SCS.Optimizer)
-    for ext in moi_file_formats
+
+    # Format lists pulled from source code of MOI.FileFormats.Model
+    # These are the formats that are compatible with this problem
+    format_ok = [".cbf", ".mof.json", ".dat-s", ".sdpa"]
+    for ext in format_ok
         @test write_to_file(problem, filename * ext) |> isnothing
+    end
+
+    # These formats are not compatible; the specific exception varies
+    format_bad = [".lp", ".mps", ".rew", ".nl"]
+    for ext in format_bad
+        @test_throws Exception write_to_file(problem, filename * ext)
     end
 end
 
 # Eric Hanson's fancy problem from issue #395
 # https://github.com/jump-dev/Convex.jl/issues/395
-let
+@testset "Fancy SDP" begin
     filename, _io = Base.Filesystem.mktemp()
 
     x = HermitianSemidefinite(2)
     problem = minimize(real(tr(x)), tr(x * [1.0 im; -im 0]) == 0, x[1, 1] == 1)
 
-    # Haven't solved the problem yet
+    # Haven't solved the problem yet, should get an ArgumentError
     @test_throws ArgumentError write_to_file(problem, filename * ".sdpa")
 
-    # Make sure file is written without error
-    # Can't check correctness since we don't provide a read_to_file function
     solve!(problem, SCS.Optimizer)
-    @test write_to_file(problem, filename * ".sdpa") |> isnothing
+
+    # Format lists pulled from source code of MOI.FileFormats.Model
+    # These are the formats that are compatible with this problem
+    format_ok = [".cbf", ".mof.json", ".dat-s", ".sdpa"]
+    for ext in format_ok
+        @test write_to_file(problem, filename * ext) |> isnothing
+    end
+
+    # These formats are not compatible; the specific exception varies
+    format_bad = [".lp", ".mps", ".rew", ".nl"]
+    for ext in format_bad
+        @test_throws Exception write_to_file(problem, filename * ext)
+    end
 end
