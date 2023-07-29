8fea7cf44edc13ba2a5f6db7c437878cf6a0ee0f
Max Kapur <maxkapur@gmail.com>
2023-07-29 09:25:29 -0400

    add quadratic example with Float64 eltype

--- test/write_to_file.jl
+++ test/write_to_file.jl
@@ -1,6 +1,36 @@
 # Tests of the write_to_file(problem, filename) interface
 
-# Simple quadratic program 
+# Simple quadratic program
+@testset "Simple quadratic program (Float64 eltype)" begin
+    filename, _io = Base.Filesystem.mktemp()
+
+    m, n = 5, 10
+    x = Variable(n)
+    A = randn(m, n)
+    b = randn(m)
+    problem::Problem{Float64} =
+        minimize(sumsquares(A * x - b), [0 <= x, x <= 1])
+
+    # Haven't solved the problem yet, should get an ArgumentError
+    @test_throws ArgumentError write_to_file(problem, filename * ".sdpa")
+
+    solve!(problem, SCS.Optimizer)
+
+    # Format lists pulled from source code of MOI.FileFormats.Model
+    # These are the formats that are compatible with this problem
+    format_ok = [".cbf", ".mof.json", ".dat-s", ".sdpa"]
+    for ext in format_ok
+        @test write_to_file(problem, filename * ext) |> isnothing
+    end
+
+    # These formats are not compatible; the specific exception varies
+    format_bad = [".lp", ".mps", ".rew", ".nl"]
+    for ext in format_bad
+        @test_throws Exception write_to_file(problem, filename * ext)
+    end
+end
+
+# Simple quadratic program, different eltype
 @testset "Simple quadratic program (Float32 eltype)" begin
     filename, _io = Base.Filesystem.mktemp()
 
