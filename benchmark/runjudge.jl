db3e6eab18b21ed54e1c34e149edc19d67df833e
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-08-01 09:46:03 +0200

    fix?

--- benchmark/runjudge.jl
+++ benchmark/runjudge.jl
@@ -14,7 +14,7 @@ group_target = benchmarkpkg(
 
 group_baseline = benchmarkpkg(
     dirname(@__DIR__),
-    mkconfig(id = "baseline"),
+    mkconfig(id = "main"),
     resultfile = joinpath(@__DIR__, "result-baseline.json"),
 )
 
