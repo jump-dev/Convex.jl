71762ff7ea78fb116ad9f8127ceff4a9c6b02c40
Benoît Legat <benoit.legat@gmail.com>
2023-12-11 18:44:59 +0100

    Fix MOI wrapper tests

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -8,22 +8,23 @@ function test_runtests()
     optimizer = MOI.Utilities.CachingOptimizer(
         MOI.Utilities.UniversalFallback(MOI.Utilities.Model{Float64}()),
         MOI.instantiate(
-            () -> Convex.Optimizer(ECOS.Optimizer),
+            () -> Convex.Optimizer(ECOS.Optimizer()),
             with_bridge_type = Float64,
         ),
     )
-    config = MOI.Test.Config(; exclude = Any[])
+    config = MOI.Test.Config(;
+        atol = 1e-4,
+        rtol = 1e-4,
+        exclude = Any[
+            MOI.ConstraintBasisStatus,
+            MOI.VariableBasisStatus,
+            MOI.ObjectiveBound,
+        ],
+    )
     return MOI.Test.runtests(
         optimizer,
         config;
-        exclude = String[
-        # GLPK issue
-        # glp_simplex: column 1: lb = 1, ub = 0; incorrect bounds
-        # glp_intopt: column 1: lb = 1, ub = 0; incorrect bounds
-        # test_constraint_ZeroOne_bounds_3: Test Failed at /home/blegat/.julia/packages/MathOptInterface/pgWRA/src/Test/test_constraint.jl:631
-        #  Expression: MOI.get(model, MOI.TerminationStatus()) == config.infeasible_status
-        #   Evaluated: MathOptInterface.INVALID_MODEL == MathOptInterface.INFEASIBLE
-            "test_constraint_ZeroOne_bounds_3",],
+        exclude = String[],
     )
 end
 
