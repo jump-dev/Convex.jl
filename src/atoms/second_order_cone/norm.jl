66c3a40bd3c36dc2c2aca3d08a58a95e5d5dbb22
Oscar Dowson <odow@users.noreply.github.com>
2024-01-11 10:45:30 +1300

    Tidy src/atoms/second_order_cone (#541)

--- src/atoms/second_order_cone/norm.jl
+++ src/atoms/second_order_cone/norm.jl
@@ -1,20 +1,22 @@
 # deprecate these soon
 norm_inf(x::AbstractExpr) = maximum(abs(x))
+
 norm_1(x::AbstractExpr) = sum(abs(x))
+
 norm_fro(x::AbstractExpr) = LinearAlgebra.norm2(vec(x))
 
 """
     norm(x::AbstractExpr, p::Real=2)
 
-Computes the `p`-norm `‖x‖ₚ = (∑ᵢ |xᵢ|^p)^(1/p)` of a vector expression `x`. Matrices
-are vectorized (i.e. `norm(x)` is the same as `norm(vec(x))`.)
+Computes the `p`-norm `‖x‖ₚ = (∑ᵢ |xᵢ|^p)^(1/p)` of a vector expression `x`.
+
+Matrices are vectorized (i.e., `norm(x)` is the same as `norm(vec(x))`.)
 
 This function uses specialized methods for `p=1, 2, Inf`. For `p > 1` otherwise,
 this function uses the procedure documented at
 [`rational_to_socp.pdf`](https://github.com/jump-dev/Convex.jl/raw/master/docs/supplementary/rational_to_socp.pdf),
 based on the paper "Second-order cone programming" by F. Alizadeh and D. Goldfarb,
 Mathematical Programming, Series B, 95:3-51, 2001.
-
 """
 function LinearAlgebra.norm(x::AbstractExpr, p::Real = 2)
     if size(x, 2) > 1
