29b10380792c199202ef6b46cebaf93aadaa52c4
odow <o.dowson@gmail.com>
2024-01-11 17:15:36 +1300

    Update tests

--- test/test_atoms.jl
+++ test/test_atoms.jl
@@ -23,6 +23,14 @@ function test_RationalNormAtom_complex_matrix()
     return
 end
 
+function test_RationalNormAtom_matrix()
+    x = Variable(2, 2)
+    atom = rationalnorm(x, 3 // 2)
+    x.value = [1.0 2.0; 3.0 4.0]
+    @test evaluate(atom) ≈ sum(abs.(x.value) .^ (3 // 2))^(2 // 3)
+    return
+end
+
 function test_RationalNormAtom_less_than_1()
     x = Variable(3)
     k = 1 // 2
