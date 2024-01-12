0dabaa4a9746708af58df65ae47622b1ae1e23bb
Oscar Dowson <odow@users.noreply.github.com>
2024-01-12 22:03:45 +1300

    Update src/deprecations.jl

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -6,7 +6,7 @@ Base.:<(lhs::AbstractExpr, rhs::Value) = <(lhs, constant(rhs))
 Base.:<(lhs::Value, rhs::AbstractExpr) = <(constant(lhs), rhs)
 
 function Base.:>(::AbstractExpr, ::AbstractExpr)
-    return error("Strict inequality `>` has been removed. Use `>` instead.")
+    return error("Strict inequality `>` has been removed. Use `>=` instead.")
 end
 Base.:>(lhs::AbstractExpr, rhs::Value) = >(lhs, constant(rhs))
 Base.:>(lhs::Value, rhs::AbstractExpr) = >(constant(lhs), rhs)
