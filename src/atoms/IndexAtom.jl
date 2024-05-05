2bba5b8aae99f48f99e2d481bf1d8f94d7de93b0
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-05-05 13:08:29 +0200

    simplify

--- src/atoms/IndexAtom.jl
+++ src/atoms/IndexAtom.jl
@@ -42,8 +42,16 @@ function evaluate(x::IndexAtom)
     return output(result)
 end
 
+function _index(tape::SparseTape{T}, keep_rows::Vector{Int}) where {T}
+    @assert issorted(keep_rows)
+    A = tape.operation.matrix
+    indexed = A[keep_rows, :]
+    af = SparseAffineOperation{T}(indexed, tape.operation.vector[keep_rows])
+    return SparseTape{T}(af, tape.variables)
+end
+_index(tape::Vector, keep_rows::Vector{Int}) = tape[keep_rows]
+
 function _index_real!(
-    context::Context{T},
     obj_size::Tuple,
     obj_tape::Union{SparseTape{T},SPARSE_VECTOR{T}},
     x::IndexAtom,
@@ -54,40 +62,18 @@ function _index_real!(
     else
         linear_indices = collect(x.inds)
     end
-    sz = length(linear_indices)
-
-    # Here, we are in the real case, so `obj_tape` is either a `SparseTape{T}`, or a `SPARSE_VECTOR`.
-    # In the latter case, we can handle it directly
-    if obj_tape isa SPARSE_VECTOR
-        return obj_tape[vec(linear_indices)]
-    end
-    # Ok, in this case we have actual work to do. We will construct an auxiliary variable `out`,
-    # which we will return, and we will constrain it to the values we want.
-    # This speeds up formulation since we reduce the problem size, and send what we have over to MOI already.
-    out = Variable(sz)
-    out_tape = conic_form!(context, out)
-    for (i, I) in enumerate(linear_indices)
-        # For each index, we constrain an element of `out` via ScalarAffineFunction to the indexed value.
-        saf = to_saf(obj_tape, I)
-        push!(saf.terms, MOI.ScalarAffineTerm(T(-1), out_tape.variables[i]))
-        MOI.Utilities.normalize_and_add_constraint(
-            context.model,
-            saf,
-            MOI.EqualTo(T(0)),
-        )
-    end
-    return out_tape
+    return _index(obj_tape, vec(linear_indices))
 end
 
 function new_conic_form!(context::Context{T}, x::IndexAtom) where {T}
     input = x.children[1]
     if !iscomplex(x) # real case
         input_tape = conic_form!(context, input)
-        return _index_real!(context, size(input), input_tape, x)
+        return _index_real!(size(input), input_tape, x)
     else # complex case
         input_tape = conic_form!(context, input)
-        re = _index_real!(context, size(input), real(input_tape), x)
-        im = _index_real!(context, size(input), imag(input_tape), x)
+        re = _index_real!(size(input), real(input_tape), x)
+        im = _index_real!(size(input), imag(input_tape), x)
         if re isa SPARSE_VECTOR
             @assert im isa SPARSE_VECTOR
             return ComplexStructOfVec(re, im)
