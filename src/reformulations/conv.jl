f2b37ddb555db767dec0882587ea85f90d8f8f68
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-05-08 00:46:41 +0200

    improve performance of `conv` (#634)
    * improve performance of `conv`
    
    * tweak
    
    * rm comment
    
    * format
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Oscar Dowson <odow@users.noreply.github.com>

--- src/reformulations/conv.jl
+++ src/reformulations/conv.jl
@@ -3,15 +3,37 @@
 # Use of this source code is governed by a BSD-style license that can be found
 # in the LICENSE file or at https://opensource.org/license/bsd-2-clause
 
+"""
+    conv1D_matrix(h::AbstractVector, n::Integer) -> SparseMatrixCSC
+
+Create a sparse matrix `A` such that if `x` has length `n`,
+then we have `A * x ≈ conv1d(h, x)`.
+"""
+function conv1D_matrix(h::AbstractVector, n::Integer)
+    m = length(h)
+    Is = Int[]
+    Js = Int[]
+    Vs = eltype(h)[]
+    sizehint!(Is, n * m)
+    sizehint!(Js, n * m)
+    sizehint!(Vs, n * m)
+    # build matrix by columns
+    for j in 1:n
+        append!(Is, j:(j+m-1))
+        append!(Js, (j for _ in 1:m))
+        append!(Vs, h)
+    end
+    return SparseArrays.sparse(Is, Js, Vs, m + n - 1, n)
+end
+
 function conv(x::Value, y::AbstractExpr)
     if length(x) != size(x, 1) || size(y, 2) > 1
         error("convolution only supported between two vectors")
     end
-    m, n = length(x), size(y, 1)
-    X = spzeros(eltype(x), m + n - 1, n)
-    for i in 1:n, j in 1:m
-        X[i+j-1, i] = x[j]
+    if length(x) == 0
+        throw(ArgumentError("convolution with empty vector not supported"))
     end
+    X = conv1D_matrix(x, length(y))
     return X * y
 end
 
