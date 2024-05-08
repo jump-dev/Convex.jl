e85899bd8b4e2e91061e9ad9cdbde379d7ac7139
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-05-09 01:09:30 +0200

    Add Aqua.jl to the tests (#642)

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be found
 # in the LICENSE file or at https://opensource.org/license/bsd-2-clause
 
-using Test
+using Test, Aqua, Convex
 
 # Seed random number stream to improve test reliability
 import Random
@@ -15,4 +15,12 @@ Random.seed!(2)
     include("test_constraints.jl")
     include("test_problem_depot.jl")
     include("MOI_wrapper.jl")
+end
+
+@testset "Aqua" begin
+    Aqua.test_all(Convex; ambiguities = false, piracies = false)
+    # Convex currently has some light piracy of `hcat`, `vcat` and `hvcat`
+    Aqua.test_piracies(Convex; treat_as_own = [hcat, vcat, hvcat])
+    # Convex currently has some ambiguities
+    Aqua.test_ambiguities(Convex; broken = true)
 end
