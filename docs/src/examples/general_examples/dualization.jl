3b9a7bd31d138a5ecce0978d3004436ee409c37f
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2024-05-12 14:56:20 +0200

    format

--- docs/src/examples/general_examples/dualization.jl
+++ docs/src/examples/general_examples/dualization.jl
@@ -20,13 +20,13 @@ using Dualization
 Random.seed!(2022)
 p = 50
 Σ = Symmetric(randn(p, p))
-Σ  = Σ * Σ'
+Σ = Σ * Σ'
 
 # Now we formulate and solve our primal problem:
 d = Variable(p)
 problem = maximize(sum(d), 0 ≤ d, d ≤ 1, Σ ⪰ Diagonal(d))
-@elapsed solve!(problem, SCS.Optimizer; silent_solver=true)
+@elapsed solve!(problem, SCS.Optimizer; silent_solver = true)
 
 # To solve the dual problem instead, we simply call `dual_optimizer` on our
 # optimizer function:
-@elapsed solve!(problem, dual_optimizer(SCS.Optimizer); silent_solver=true)
+@elapsed solve!(problem, dual_optimizer(SCS.Optimizer); silent_solver = true)
