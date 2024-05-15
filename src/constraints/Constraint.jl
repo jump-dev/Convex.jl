26244b1347497313a50c608679d2860bf4428f40
Benoît Legat <benoit.legat@gmail.com>
2024-05-15 10:41:56 +0200

    Remove abstract type Constraint

--- src/constraints/Constraint.jl
+++ src/constraints/Constraint.jl
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be found
 # in the LICENSE file or at https://opensource.org/license/bsd-2-clause
 
-mutable struct Constraint{S<:MOI.AbstractSet} <: Constraint
+mutable struct Constraint{S<:MOI.AbstractSet}
     child::AbstractExpr
     set::S
     dual::Union{Value,Nothing}
