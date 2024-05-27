070edb0ae1aea0a11249bd6523e63f81e5500aa6
odow <o.dowson@gmail.com>
2024-05-27 20:05:09 +1200

    Add dims argument to LogSumExpAtom

--- src/atoms/LogSumExpAtom.jl
+++ src/atoms/LogSumExpAtom.jl
@@ -3,17 +3,26 @@
 # Use of this source code is governed by a BSD-style license that can be found
 # in the LICENSE file or at https://opensource.org/license/bsd-2-clause
 
+"""
+    LogSumExpAtom(x::AbstractExpr, dims::Int = 0)
+
+Represents the expression `log.(sum(exp.(x); dims))`.
+"""
 mutable struct LogSumExpAtom <: AbstractExpr
     children::Tuple{AbstractExpr}
     size::Tuple{Int,Int}
+    dims::Union{Colon,Int}
 
-    function LogSumExpAtom(x::AbstractExpr)
+    function LogSumExpAtom(x::AbstractExpr, dims::Union{Colon,Int} = :)
+        @assert dims == Colon() || 1 <= dims <= 2
         if sign(x) == ComplexSign()
             error(
                 "[LogSumExpAtom] the argument should be real but it's instead complex",
             )
         end
-        return new((x,), (1, 1))
+        m = dims == 2 ? size(x, 1) : 1
+        n = dims == 1 ? size(x, 2) : 1
+        return new((x,), (m, n), dims)
     end
 end
 
@@ -28,16 +37,23 @@ curvature(::LogSumExpAtom) = ConvexVexity()
 function evaluate(x::LogSumExpAtom)
     _x = evaluate(x.children[1])
     max_x = maximum(_x)
-    return max_x + log(sum(exp.(_x .- max_x)))
+    return max_x .+ log.(sum(exp.(_x .- max_x); x.dims))
 end
 
-logsumexp(x::AbstractExpr) = LogSumExpAtom(x)
+logsumexp(x::AbstractExpr; dims = Colon()) = LogSumExpAtom(x, dims)
 
 function new_conic_form!(context::Context, e::LogSumExpAtom)
     # log(sum(exp(x))) <= t  <=>  sum(exp(x)) <= exp(t) <=> sum(exp(x - t)) <= 1
-    t = Variable()
-    z = sum(exp(e.children[1] - t * ones(size(e.children[1]))))
-    add_constraint!(context, 1 >= z)
+    x = only(e.children)
+    t = Variable(size(e))
+    y = if e.dims == 1  # t is a row-vector
+        ones(size(x, 1), 1) * t
+    elseif e.dims == 2  # t is a col-vector
+        t * ones(1, size(x, 2))
+    else
+        t * ones(size(x))
+    end
+    add_constraint!(context, 1 >= sum(exp(x - y); dims = e.dims))
     return conic_form!(context, t)
 end
 
@@ -45,5 +61,5 @@ function logisticloss(e::AbstractExpr)
     if length(e) == 1
         return logsumexp([e; 0])
     end
-    return sum(logsumexp([e[i]; 0]) for i in 1:length(e))
+    return sum(logsumexp(hcat(vec(e), zeros(length(e))); dims = 2))
 end
