f28f6434dbbba5db7aa10b3403b8a249b92de65b
Documenter.jl <documenter@juliadocs.github.io>
2024-12-04 02:38:29 +0000

    delete history

---
+++ dev/examples/optimization_with_complex_variables/povm_simulation.jl
@@ -0,0 +1,86 @@
+# # POVM simulation
+# This notebook shows how we can check how much depolarizing noise a qubit positive operator-valued measure (POVM) can take before it becomes simulable by projective measurements. The general method is described in [arXiv:1609.06139](https://arxiv.org/abs/1609.06139). The question of simulability by projective measurements boils down to an SDP problem. Eq. (8) from the paper defines the noisy POVM that we obtain subjecting a POVM $\mathbf{M}$ to a depolarizing channel $\Phi_t$:
+#
+# ```math
+# \left[\Phi_t\left(\mathbf{M}\right)\right]_i := t M_i + (1-t)\frac{\mathrm{tr}(M_i)}{d} \mathbb{1}.
+# ```
+#
+# If this visibility $t\in[0,1]$ is one, the POVM $\mathbf{M}$ is simulable.
+#
+# We will use Convex.jl to solve the SDP problem.
+
+using Convex, SCS, LinearAlgebra
+
+# For the qubit case, a four outcome qubit POVM $\mathbf{M} \in\mathcal{P}(2,4)$ is simulable if and only if
+#
+# $M_{1}=N_{12}^{+}+N_{13}^{+}+N_{14}^{+},$
+#
+# $M_{2}=N_{12}^{-}+N_{23}^{+}+N_{24}^{+},$
+#
+# $M_{3}=N_{13}^{-}+N_{23}^{-}+N_{34}^{+},$
+#
+# $M_{4}=N_{14}^{-}+N_{24}^{-}+N_{34}^{-},$
+#
+# where Hermitian operators $N_{ij}^{\pm}$ satisfy $N_{ij}^{\pm}\geq0$ and $N_{ij}^{+}+N_{ij}^{-}=p_{ij}\mathbb{1}$, where $i<j$ , $i,j=1,2,3,4$ and $p_{ij}\geq0$ as well as $\sum_{i<j}p_{ij}=1$, that is, the $p_{ij}$ values form a probability vector. This forms an SDP feasibility problem, which we can rephrase as an optimization problem by adding depolarizing noise to the left-hand side of the above equations and maximizing the visibility $t$:
+#
+# $\max_{t\in[0,1]} t$
+#
+# such that
+#
+# $t\,M_{1}+(1-t)\,\mathrm{tr}(M_{1})\frac{\mathbb{1}}{2}=N_{12}^{+}+N_{13}^{+}+N_{14}^{+},$
+#
+# $t\,M_{2}+(1-t)\,\mathrm{tr}(M_{2})\frac{\mathbb{1}}{2}=N_{12}^{-}+N_{23}^{+}+N_{24}^{+},$
+#
+# $t\,M_{3}+(1-t)\,\mathrm{tr}(M_{3})\frac{\mathbb{1}}{2}=N_{13}^{-}+N_{23}^{-}+N_{34}^{+},$
+#
+# $t\,M_{4}+(1-t)\,\mathrm{tr}(M_{4})\frac{\mathbb{1}}{2}=N_{14}^{-}+N_{24}^{-}+N_{34}^{-}$.
+#
+# We organize these constraints in a function that takes a four-output qubit POVM as its argument:
+
+function get_visibility(K)
+    noise = real([tr(K[i]) * I(2) / 2 for i in 1:size(K, 1)])
+    P = [[ComplexVariable(2, 2) for i in 1:2] for j in 1:6]
+    q = Variable(6, Positive())
+    t = Variable(1, Positive())
+    constraints = Constraint[isposdef(P[i][j]) for i in 1:6 for j in 1:2]
+    push!(constraints, sum(q) == 1)
+    push!(constraints, t <= 1)
+    append!(constraints, [P[i][1] + P[i][2] == q[i] * I(2) for i in 1:6])
+    push!(
+        constraints,
+        t * K[1] + (1 - t) * noise[1] == P[1][1] + P[2][1] + P[3][1],
+    )
+    push!(
+        constraints,
+        t * K[2] + (1 - t) * noise[2] == P[1][2] + P[4][1] + P[5][1],
+    )
+    push!(
+        constraints,
+        t * K[3] + (1 - t) * noise[3] == P[2][2] + P[4][2] + P[6][1],
+    )
+    push!(
+        constraints,
+        t * K[4] + (1 - t) * noise[4] == P[3][2] + P[5][2] + P[6][2],
+    )
+    p = maximize(t, constraints)
+    return solve!(p, SCS.Optimizer; silent = true)
+end
+
+# We check this function using the tetrahedron measurement (see Appendix B in [arXiv:quant-ph/0702021](https://arxiv.org/abs/quant-ph/0702021)). This measurement is non-simulable, so we expect a value below one.
+
+function dp(v)
+    return I(2) + v[1] * [0 1; 1 0] + v[2] * [0 -im; im 0] + v[3] * [1 0; 0 -1]
+end
+b = [
+    1 1 1
+    -1 -1 1
+    -1 1 -1
+    1 -1 -1
+] / sqrt(3)
+M = [dp(b[i, :]) for i in 1:size(b, 1)] / 4;
+p = get_visibility(M)
+
+#-
+p.optval
+
+# This value matches the one [we obtained](https://github.com/peterwittek/ipython-notebooks/blob/master/Simulating_POVMs.ipynb) using [PICOS](http://picos.zib.de/).
---
+++ previews/PR605/examples/general_examples/chebyshev_center.jl
@@ -0,0 +1,49 @@
+# # Chebyshev center
+# Boyd & Vandenberghe, "Convex Optimization"
+# Joëlle Skaf - 08/16/05
+#
+# Adapted for Convex.jl by Karanveer Mohan and David Zeng - 26/05/14
+#
+# The goal is to find the largest Euclidean ball (that is, its center and
+# radius) that lies in a polyhedron described by affine inequalities in this
+# fashion: $P = \{x : a_i'*x \leq b_i, i=1,\ldots,m \}$ where $x \in \mathbb{R}^2$.
+
+using Convex
+using LinearAlgebra
+import SCS
+
+# Generate the input data
+a1 = [2; 1];
+a2 = [2; -1];
+a3 = [-1; 2];
+a4 = [-1; -2];
+b = ones(4, 1);
+
+# Create and solve the model
+r = Variable(1)
+x_c = Variable(2)
+p = maximize(r)
+p.constraints += a1' * x_c + r * norm(a1, 2) <= b[1];
+p.constraints += a2' * x_c + r * norm(a2, 2) <= b[2];
+p.constraints += a3' * x_c + r * norm(a3, 2) <= b[3];
+p.constraints += a4' * x_c + r * norm(a4, 2) <= b[4];
+solve!(p, SCS.Optimizer; silent_solver = true)
+p.optval
+
+# Generate the figure
+x = range(-1.5, stop = 1.5, length = 100);
+theta = 0:pi/100:2*pi;
+using Plots
+plot(x, x -> -x * a1[1] / a1[2] + b[1] / a1[2])
+plot!(x, x -> -x * a2[1] / a2[2] + b[2] / a2[2])
+plot!(x, x -> -x * a3[1] / a3[2] + b[3] / a3[2])
+plot!(x, x -> -x * a4[1] / a4[2] + b[4] / a4[2])
+plot!(
+    evaluate(x_c)[1] .+ evaluate(r) * cos.(theta),
+    evaluate(x_c)[2] .+ evaluate(r) * sin.(theta),
+    linewidth = 2,
+)
+plot!(
+    title = "Largest Euclidean ball lying in a 2D polyhedron",
+    legend = nothing,
+)
