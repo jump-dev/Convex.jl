77ed3e08944489f70284e93e4975dd932037d097
odow <o.dowson@gmail.com>
2025-01-06 10:24:05 +1300

    Add support for Base.log1p

---
+++ src/reformulations/log1p.jl
@@ -0,0 +1,6 @@
+# Copyright (c) 2014: Madeleine Udell and contributors
+#
+# Use of this source code is governed by a BSD-style license that can be found
+# in the LICENSE file or at https://opensource.org/license/bsd-2-clause
+
+Base.log1p(x::AbstractExpr) = log(1 + x)
